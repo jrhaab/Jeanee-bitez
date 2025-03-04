7af4d16341c8245b8e9b061b2456acc34b608d2f
jayCde <jeff.k.asmah@outlook.com>
2025-03-04 14:45:57 +0000

    ui/ux: refine landing and auth choice screens, add new and fallback asets

--- pages/auth-choice.js
+++ pages/auth-choice.js
@@ -6,17 +6,28 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Authchoice() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`bg-[url('/bg/jenee-auth-bg.png')] bg-no-repeat bg-cover bg-center flex flex-col justify-end items-end pb-0`}
     >
-   
-     <Image src={"https://res.cloudinary.com/dfdy55ndo/image/upload/v1735923906/jeanee-transparent-logo_wz8kzv.png"} alt="" width={100} height={100} />
-     <span className="mb-40">Do you want to sign up or login?</span> <br />
-     <div className="flex flex-col gap-10">
-     <button className=" bg-red-800 justify-center rounded-2xl text-white h-10 flex items-center w-52">Sign up </button>
-     <button className=" bg-red-800 justify-center rounded-2xl  text-white h-10 flex items-center w-52">Login </button>
-     </div>
-     
-
+      <section className="w-full h-max rounded-t-[var(--radius-md)] bg-white opacity-95 flex flex-col items-center justify-center gap-y-16 px-10 py-10">
+        <Image
+          src={"/trademarks/jeanee-transparent-logo_wz8kzv.png"}
+          alt=""
+          width={1024}
+          height={1024}
+          className="w-1/2 h-auto place-self-center"
+        />
+        <span className="text-black font-medium text-base text-center w-full">
+          Login or Sign Up to get the most out of Jeanee.
+        </span>{" "}
+        <div className="flex flex-col justify-center items-center gap-y-4 w-full">
+          <button className="bg-[var(--secondary-red-color)] rounded-[var(--radius-full)] text-white text-2xl font-bold tracking-[0.05rem] flex flex-col items-center justify-center px-4 py-3 w-full">
+            Sign up
+          </button>
+          <button className="bg-[var(--primary-red-color)] rounded-[var(--radius-full)] text-white text-2xl font-bold tracking-[0.05rem] flex flex-col items-center justify-center px-4 py-3 w-full">
+            Login
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
 import { Inter } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Landingpage() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`bg-[url('/bg/pexels-yuuilina-10254480.jpg')] bg-no-repeat bg-cover bg-center flex flex-col justify-end items-end pb-0`}
     >
-   
-     <Image src={"https://res.cloudinary.com/dfdy55ndo/image/upload/v1735923906/jeanee-transparent-logo_wz8kzv.png"} alt="" width={90} height={90} />
-     <span className="mb-40">The best diary ordering and delivery app of the century </span> <br />
-     <button className=" bg-red-800 justify-center ml-1 rounded-2xl h-10 text-white flex items-center w-52">Next</button>
-
+      <section className="w-full h-max rounded-t-[var(--radius-md)] bg-white opacity-95 flex flex-col items-center justify-center gap-y-16 px-10 py-10">
+        <Image
+          src={"/trademarks/jeanee-transparent-logo_wz8kzv.png"}
+          alt=""
+          width={1024}
+          height={1024}
+          className="w-1/2 h-auto place-self-center"
+        />
+        <div className="w-full h-max flex flex-col items-center justify-center  !gap-y-4 text-center">
+          <span className="text-black font-medium text-base text-center w-full">
+            The best diary ordering and delivery app of the century.
+          </span>
+          <Link
+            href={"/auth-choice"}
+            className="bg-[var(--secondary-red-color)] rounded-[var(--radius-full)] text-white text-2xl font-bold tracking-[0.05rem] flex flex-col items-center justify-center px-4 py-3 w-full"
+          >
+            Get started
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
